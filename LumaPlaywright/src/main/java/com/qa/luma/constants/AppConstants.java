08ee628759b0cd37022bf706f423135a869e8623
BASILAHAMED <sbasil.ahamed@gmail.com>
2024-01-19 14:29:38 +0530

    first commit

---
+++ LumaPlaywright/src/main/java/com/qa/luma/constants/AppConstants.java
@@ -0,0 +1,11 @@
+package com.qa.luma.constants;
+
+public class AppConstants {
+	
+	
+	public static final String HOME_PAGE_TITLE = "Home Page";
+	
+	public static final String LOGIN_PAGE_TITLE = "Home Page";
+
+
+}
